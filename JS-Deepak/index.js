6be7a10c4b2a4036b10887c1add9b8ea32453424
dipeshjoshi <dipeshj2310@gmail.com>
2024-02-22 13:04:14 +0530

    its committ

---
+++ JS-Deepak/index.js
@@ -0,0 +1,7 @@
+//Number Is Not Prime Or Not
+
+//What is Prime Number ? =>
+
+// git remote add origin https://github.com/dipeshjoshi4/JavaScript.git
+// git branch -M main
+// git push -u origin main
