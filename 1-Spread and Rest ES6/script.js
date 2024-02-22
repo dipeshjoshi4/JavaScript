82c50b548419a806642def1fc6b18f893f63a30f
dipeshjoshi <dipeshj2310@gmail.com>
2024-02-22 14:52:48 +0530

    thsi is the old code

---
+++ 1-Spread and Rest ES6/script.js
@@ -0,0 +1,16 @@
+//----------------------------------------------ES6 Rest & Spread Operator------------------------------
+
+//----------------------------------------------Rest Operator-------------------------------------------
+
+//Normal Function
+// function addNumbers(a, b, c) {
+//   return a + b + c;
+// }
+// const result = addNumbers(2, 5, 6);
+// console.log(result);
+
+function addNumbers(a, b, c) {
+  return a + b + c;
+}
+const result = addNumbers(2, 5, 6, 8, 9);
+console.log(result);
