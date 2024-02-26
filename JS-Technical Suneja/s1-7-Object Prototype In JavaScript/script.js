174c8c8c7468b8a60c7f7076ca7805efdeb56630
dipeshjoshi <dipeshj2310@gmail.com>
2024-02-26 12:34:13 +0530

    added prototype object in js

---
+++ JS-Technical Suneja/s1-7-Object Prototype In JavaScript/script.js
@@ -0,0 +1,24 @@
+console.log("Object Prototype In JavaScript");
+
+//---------------------------------------------JS Prototype perks----------------------
+
+//CUTOM FUNC MADE BY
+//EVERY INBUILT PUSH..... TYPE METHODS
+//INHERITANCE ALL BECASUE PROTOTYPE
+
+//inheritance means u have class that class contains variable and function and u have to inherit that members or function and u do that .that is called inheritance
+//inhertance means on e clas property u use in another class thtas called inheritance
+
+//js object made by this
+
+const obj1 = new Object({
+    name: "Ajay";
+});
+
+//2nd
+const objName = new objName();
+
+//3rd
+const obj2 = {
+    name: "Ajay"
+};
