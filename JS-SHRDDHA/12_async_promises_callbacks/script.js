b6fd7a2ae8ac45d3da326ce5a5c7a87d8937cbfc
dipeshjoshi <dipeshj2310@gmail.com>
2024-03-05 15:24:54 +0530

    async await promises callback callbackHell

--- JS-SHRDDHA/12_async_promises_callbacks/script.js
+++ JS-SHRDDHA/12_async_promises_callbacks/script.js
@@ -3,7 +3,7 @@ console.log("callback_promise_async_await");
 //--------------------------------------------------------------------------------------------------
 //-------------------------callback_callbackHell_promise_promiseChaining_async_await----------------
 //--------------------------------------------------------------------------------------------------
-
+//////////////////////////////////////////
 //async await >> promise chains >> callback hell
 
 //-------------------async programing--------------------------
@@ -97,14 +97,14 @@ console.log("callback_promise_async_await");
 
 // so one by one with some delay we have  need  to get our value.for that we use callbacks
 
-function getData(dataId, getNextData) {
-  setTimeout(() => {
-    console.log("data", dataId);
-    if (getNextData) {
-      getNextData();
-    }
-  }, 2000);
-}
+// function getData(dataId, getNextData) {
+//   setTimeout(() => {
+//     console.log("data", dataId);
+//     if (getNextData) {
+//       getNextData();
+//     }
+//   }, 2000);
+// }
 
 //- above after our getdata function done,then we pass another getnextdata function which give another value
 
@@ -122,21 +122,371 @@ function getData(dataId, getNextData) {
 
 //this is callbackHell => which is not understable at first and pro developers so that
 
-getData(1, () => {
-  console.log("getting data2...");
-  getData(2, () => {
-    console.log("getting data3...");
-    getData(3, () => {
-      console.log("getting data4...");
-      getData(4);
-    });
-  });
-});
+
+
+// function getData(dataId, getNextData) {
+//   setTimeout(() => {
+//     console.log("data", dataId);
+//     if (getNextData) {
+//       getNextData();
+//     }
+//   }, 2000);
+// }
+// getData(1, () => {
+//   console.log("getting data2...");
+//   getData(2, () => {
+//     console.log("getting data3...");
+//     getData(3, () => {
+//       console.log("getting data4...");
+//       getData(4);
+//     });
+//   });
+// });
 
 // - this tsyle of programing difficult to understand and manage
 
 //--------------------------------------------------------------------------------------------------
-//-----------------------------------------------Promises---------------------------------------
+//-----------------------------------------------Promises Basic---------------------------------------
 //--------------------------------------------------------------------------------------------------
 
 // callback is using in nested format and get callbackhell problem and now solution of callback hell we get the promises
+
+// Promise is for "eventual" completion of task.it is an object in JS.its a solution to callbackhell
+
+// let promise = new Promise((resolve, reject) => { })      //-------------------function with two handlers
+
+//resolve & rejct will be callback provider
+
+//promise have 3 state => pending || fullfilled(resolved) || rejcted
+
+
+// let promise = new Promise((resolve, reject) => {
+//   console.log("success");
+//   resolve("success got it");
+// })
+// console.log(promise) //promisestate => fullfilled
+
+// let promise2 = new Promise((resolve, reject) => {
+//   console.log("error");
+//   reject("error got it")
+// })
+// console.log(promise2); //promisestate => rejected
+
+// let promise3 = new Promise((resolve, reject) => {
+//   console.log(123)
+// })
+// console.log(promise3);  //promisestate => pending
+
+
+//promise object | 3 result |resolve and reject -handler |with this we can gerenate final result or error
+//when we request api for data then the api give us promisereturn and then promise se data and result and state niklta hai
+
+
+//-----------------------GENERAL PROGRAMMING JS USE of Promise -------------------------------------
+
+// function getData(dataid, getNextData) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("data", dataid);
+//       resolve("success");
+//       // if (getNextData) {
+//       //   getNextData()
+//       // }
+//     }, 5000)
+//   });
+// }
+
+//do this in console to Show Above Result for clearity and understanding
+// let promise = getData(1);
+// promise => pending
+// answer will be print
+// promise => success
+
+
+//A JS Promise Object can be
+ //- pending : the result is undefined
+ //- resolve : the result have some value (fulfilled)  // resolve(result)
+ //- reject : the result is error object               // rejcet(error)
+
+
+//-----------------------------------------------------------------------------------------------------
+ //we learn how promise will be worked,but API give the promise we just have to handled 
+ //Promise onece we get how we can use of promise get data or play with them fot hat we have two method 
+ 
+//  ||promise.then((res)=>{})   //fullfilled
+//  ||promise.catch((err)=>{})
+
+
+
+//FOR RESOLVE PROMISE
+
+// const getPromise = () => {
+//   return new Promise((resolve, reject) => {
+//     console.log("i am promise");
+//     resolve("success");
+//   });
+// }
+
+// let promise = getPromise();
+// promise.then((res) => {
+//   console.log("promise fulfilled",res)
+// })
+
+//FOR REJECT PROMISE
+
+// const promise1Fun = () => {
+//   return new Promise((resolev, reject) => {
+//     console.log("i am promise1");
+//     reject("network error show")
+//   })
+// }
+
+// let promise1ans = promise1Fun()
+//   promise1ans.catch((err) => {
+//   console.log("promise reject",err)
+// })
+
+//--------------------------------------------------------------------------------------------------
+//-----------------------------------------------Promises_Chaining----------------------------------
+//--------------------------------------------------------------------------------------------------
+
+//let assume we have some async function like this below
+
+// function asynFunc() {
+//   return new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//       console.log("Data-1")
+//       resolve("success ")
+//     },4000)
+//   })
+// }
+
+// console.log("fetching data 1...")
+
+// let p1 = asynFunc();
+// p1.then((res) => {
+//   console.log(res)
+// })
+
+//the nromal way of runing promises but we have to do only last part of the promise so that we can get our data from the promise
+
+//now want the first username check and then password check .so i am doing first data 1 and then data 2
+
+// function asynFunc1() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Data-1");
+//       resolve("success ");
+//     }, 4000);
+//   });
+// }
+
+// function asynFunc2() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Data-2");
+//       resolve("success ");
+//     }, 4000);
+//   });
+// }
+
+//  console.log("fetching data 1...");
+// let p1 = asynFunc1();
+// p1.then((res) => {
+//  console.log("fetching data 2...");
+//   let p2 = asynFunc2();
+//   p2.then((res) => { })
+// });
+
+///we can simplifying above function like this too
+
+//  console.log("fetching data 1...");
+//  asynFunc1().then((res) => {
+//    console.log("fetching data 2...");
+//    asynFunc2().then((res) => {});
+//  });
+
+
+//---------------------------------------THIS IS FOR MY PRACTISE OF ABOVE EXAMPLE ------------------------------------------------
+
+/*
+
+function asynFunction1() {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      console.log("data-1")
+      resolve("data-1 got success")
+    },4000)
+  })
+}
+
+function asyncFunction2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data-2")
+      resolve("data-2 got success")
+    },4000)
+  })
+}
+
+
+console.log("fetching data-1......")
+asynFunction1().then((res) => {
+  console.log(res)
+    console.log("fetching data-2......");
+  asyncFunction2().then((res) => {
+    console.log(res)
+  });
+})
+
+*/
+
+//---------------------------------------THIS IS FOR MY PRACTISE OF ABOVE EXAMPLE ------------------------------------------------
+
+
+//IN STARTING CALLBACK HELL CODE CONVERT INTO PROMISING CHAINING
+
+//CALLBACK HELL SOLUTION WICH IS COMPLEX AND NOT EASY UNDERSTAND
+// function getData(dataId, getNextData) {
+//   setTimeout(() => {
+//     console.log("data", dataId);
+//     if (getNextData) {
+//       getNextData();
+//     }
+//   }, 2000);
+// }
+// getData(1, () => {
+//   console.log("getting data2...");
+//   getData(2, () => {
+//     console.log("getting data3...");
+//     getData(3, () => {
+//       console.log("getting data4...");
+//       getData(4);
+//     });
+//   });
+// });
+
+//Our Promise SOLUTION
+
+// function getData(dataId /*getNextData*/) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("data", dataId);
+//       resolve("success")
+//       // if (getNextData) {
+//       //   getNextData();
+//       // }
+//     }, 3000);
+//   })
+// }
+
+//optimize -2
+// getData(1).then((res) => {
+//   console.log(res);
+//   getData(2).then((res) => {
+//     console.log(res);
+//     getData(3).then((res) => {
+//       console.log(res)
+//     })
+//   })
+// })
+
+//otimize-3
+
+// console.log("getting data-1...............")
+// getData(1)
+//   .then((res) => {
+//     console.log("getting data-2...............");
+//     return getData(2);
+//   })
+//   .then((res) => {
+//     console.log("getting data-3...............");
+//     return getData(3)
+//   })
+//   .then((res) => {
+//     console.log("getting data-4...............");
+//     return getData(4)
+//   })
+//   .then((res) => {
+//      console.log(res);
+//   })
+  
+
+//--------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------Aync Await-----------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+
+// async function always returns a promise
+// async keyword use with function 
+// awaits pause the execution of its surriunding async function untill the promise is settled
+
+//syntax
+// async function myFunc(){}
+
+//EXAMPLE-1
+
+// function api() {
+//   return new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//       console.log("wether data");
+//       resolve(200)
+//     },2000)
+//   })
+// }
+
+// await api(); //want to call promise by this awiat only use in async function in so we write like this
+// async function getWeatherData() {
+//   await api(); //untill this call nothing below this executed so we can execute other promise but one by one
+//   await api()
+// }
+// console.log(getWeatherData());
+
+//EXAMPLE-2
+function getData(dataId ) {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      console.log("data", dataId);
+      resolve("success")
+    }, 2000);
+  })
+}
+
+// async function getAllData() {
+//   console.log("getting wait for data-1.........")
+//   await getData(1);
+//   console.log("getting wait for data-2.........");
+//   await getData(2);
+//   console.log("getting wait for data-3.........");
+//   await getData(3);
+//   console.log("getting wait for data-4.........");
+//   await getData(4);
+// }
+
+// console.log(getAllData());
+
+//=> so in async await wee need to call for the run program function call needed for that once we have to use
+
+//IIFE => immediately invoked function expression
+//IIFE IS FUNCTION THAT IS CALL TO IMMEDIATELY AFTER THE DEFINE
+//IIFE is use only once
+
+// syntax
+
+//all 3 syntax bu there is NO FUNCTION NAME IN IIFE SYNTAX
+//1.(func)()
+//2.(()=>{})()
+//3.(async func)()
+
+//example
+
+// (async function () {
+//   console.log("getting wait for data-1.........")
+//   await getData(1);
+//   console.log("getting wait for data-2.........");
+//   await getData(2);
+//   console.log("getting wait for data-3.........");
+//   await getData(3);
+//   console.log("getting wait for data-4.........");
+//   await getData(4);
+// }) ()
+
