aee9143631aef2e9bceb853501599bf65d7485d2
dipeshjoshi <dipeshj2310@gmail.com>
2024-03-29 22:43:10 +0530

    thapa technical question-1

--- JS-Thapa/1.js
+++ JS-Thapa/1.js
@@ -3,16 +3,35 @@
 
 //cond - whitspace ignore at start and end || and length will be not 0
 
+//1st way
 
+// const findlongestword = (str) => {
+//     if (str.trim().length === 0) {
+//         return false;
+//     }
+//     words = str.split(" "); //by space word differ
+//     words = words.sort((a, b) => b.length - a.length)
+//     return words[0];
+// }
+
+// console.log(
+//     findlongestword("Watch Thapa Technical javascript course on youtube")
+// )
+
+
+//2nd way
 const findlongestword = (str) => {
     if (str.trim().length === 0) {
         return false;
     }
-    words = str.split(" "); //by space word differ
-    words = words.sort((a, b) => b.length - a.length)
-    return words[0];
+    words = str.split(" ");
+    return words.reduce(
+        (acc, curvalue) => (curvalue.length > acc.length ? curvalue : acc), " "
+    )
 }
 
 console.log(
-    findlongestword("Watch Thapa Technical javascript course on youtube")
-)
+    findlongestword(
+        ("Watch Thapa Technical javascript course on youtube")
+    )
+);
