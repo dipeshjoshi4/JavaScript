f8fdd34923e515020f6322821c4f21ddd8284e1f
dipeshjoshi <dipeshj2310@gmail.com>
2024-04-21 16:39:15 +0530

    chnage

--- JS-thapa/full file of thapa/6_arrays.js
+++ JS-thapa/full file of thapa/6_arrays.js
@@ -9,7 +9,7 @@
 
 //* JavaScript Array is a data structure that allows you to store and organize multiple values within a single variable. It is a versatile and dynamic object. It can hold various data types, including numbers, strings, objects, and even other arrays. Arrays in JavaScript are zero-indexed i.e. the first element is accessed with an index 0, the second element with an index of 1, and so forth.
 
-//* ======================================
+//* ======================================+
 //*  Creating Arrays:
 //* =====================================
 
