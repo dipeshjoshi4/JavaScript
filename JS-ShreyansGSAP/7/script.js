281dec16afcf2f30d5a0fda6c9ce7e1f82bd1773
dipeshjoshi <dipeshj2310@gmail.com>
2024-05-03 09:46:21 +0530

    add

--- JS-ShreyansGSAP/7/script.js
+++ JS-ShreyansGSAP/7/script.js
@@ -1,1 +1,49 @@
 
+
+
+
+
+window.addEventListener("wheel", function (event) {
+    if (event.deltaY > 0) {
+        gsap.to(".marque", {
+            transform: "translateX(-200%)",
+            repeat: -1,
+            duration: 4,
+            ease: 'none'
+        });
+        gsap.to(".marque img", { rotate: 180 });
+    } else {
+        gsap.to(".marque", {
+            transform: "translateX(0%)",
+            repeat: -1,
+            duration: 4,
+            ease: 'none'
+        });
+        gsap.to(".marque img", { rotate: 0 });
+    }
+});
+
+
+//ERROR CODE which couldnt work
+
+// window.addEventListener("wheel", function (dets) {
+//     if (dets.deltay > 0) {
+//         gsap.to(".marque", {
+//             transform: "translateX(-200%)",
+//             repeat: -1,
+//             duration: 4,
+//             ease: 'none'
+//         })
+//         gsap.to(".marque img", { rotate: 180 })
+//     }
+//     else {
+//         gsap.to(".marque", {
+//             transform: "translateX(0%)",
+//             repeat: -1,
+//             duration: 4,
+//             ease: 'none'
+//         })
+//         gsap.to(".marque img", { rotate: 0 })
+
+//     }
+// })
